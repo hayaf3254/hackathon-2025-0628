f59d390e1eaf652ec620c3e6b47215c14f317c2a
kureo1111 <kureo3102@gmail.com>
2025-06-28 17:06:06 +0900

    ルーティング設定しました。

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,19 @@
+const express = require("express");
+const app = express();
+const userRouter = require("./routes/user");
 
-console.log(`サーバーが起動しました`);
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    // console.log("hello express");
+    // res.send("<h1>こんにちは</h1>");
+    // res.sendStatus(500);
+    // res.status(500).send("エラーです。");
+    res.status(500).json({ msg: "エラーです。" });
+});
+
+//ルーティング
+app.use("/user", userRouter);
+
+
+app.listen(PORT, () => console.log("サーバーが起動しました"));
